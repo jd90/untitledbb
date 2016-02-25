d65f7554fe78ad40f858e588013b526e092250c2
jd90 <1418146@rgu.ac.uk>
2016-02-25 20:20:23 +0000

    dsa

--- ownerResult.php
+++ ownerResult.php
@@ -68,7 +68,7 @@ try {
 $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 try{
-    $st = $conn-> query("SELECT * FROM [Customer]");
+    $st = $conn-> query("SELECT * FROM [Owner]");
     foreach($st->fetchAll() as $row) {
         $newhtml =
             <<<NEWHTML
